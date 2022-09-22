9d2315972e60680d071753c84ee005cf350613da
Nebil Ağgün <108563288+aggun@users.noreply.github.com>
2022-09-22 10:40:06 +0300

    Update PasswordHashingSaltingTest.cs

--- BackEnd/PycApi.TestX/PasswordHashingSaltingTest.cs
+++ BackEnd/PycApi.TestX/PasswordHashingSaltingTest.cs
@@ -15,7 +15,6 @@ namespace PycApi.TestX
             byte[] passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
 
             var hmac1 = new System.Security.Cryptography.HMACSHA512(passwordSalt);
-
             var computedPasswordHash = hmac1.ComputeHash(Encoding.UTF8.GetBytes(password));
 
             Assert.Equal(passwordHash, computedPasswordHash);
